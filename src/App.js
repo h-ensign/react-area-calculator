9d8af09ca09259719039f29c4555cd2bddbdf984
alensign <hassan.ensign@gmail.com>
2022-12-18 18:59:49 +0300

    deleted unused files. implemented calc logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Button, Typography, TextField } from "@mui/material";
+import { Form } from "./App.styles";
 
 function App() {
+  const [radius, setRadius] = useState("");
+  const [volume, setVolume] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (radius < 0) {
+      alert("Invalid input. Positive values only");
+    } else {
+      let result = (4 / 3) * Math.PI * radius ** 3;
+      setVolume(result.toFixed(2));
+    }
+  };
+  const handleRadiusChange = (e) => {
+    setRadius(e.target.value);
+  };
+  const handleReload = () => {
+    setRadius("");
+    setVolume("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container">
+        <Typography
+          variant="h6"
+          sx={{
+            width: 250,
+            mb: 2,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Sphere Volume Calculator
+        </Typography>
+        <Form onSubmit={handleSubmit}>
+          <TextField
+            sx={{
+              width: 100,
+              mb: 2,
+            }}
+            variant="outlined"
+            label="Radius"
+            id="outlined-basic"
+            value={radius}
+            onChange={handleRadiusChange}
+          />
+
+          <Button
+            variant="contained"
+            type="submit"
+            sx={{
+              width: 100,
+              mb: 2,
+            }}
+          >
+            Submit
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={handleReload}
+            sx={{
+              width: 100,
+              mb: 2,
+            }}
+          >
+            Reload
+          </Button>
+        </Form>
+        <Typography variant="h6">Volume is {volume}</Typography>
+      </div>
     </div>
   );
 }
---
+++ src/App.styles.js
@@ -0,0 +1,12 @@
+import { styled } from "@mui/material";
+
+export const Form = styled("form")({
+  display: "flex",
+  flexDirection: "column",
+  width: 100,
+  margin: "auto",
+  marginTop: 2,
+  marginBottom: 2,
+  alignContent: "center",
+  justifyContent: "center",
+});
